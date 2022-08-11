2fff0f905749a0ff25325d740a69b10e7c535333
Pro-Tech <mehmood123rehman@gmail.com>
2022-08-11 16:34:32 +0500

    updated names of methods and varriables

--- app/src/main/java/com/example/s3supermart/Activity/HomeActivity.java
+++ app/src/main/java/com/example/s3supermart/Activity/HomeActivity.java
@@ -2,9 +2,15 @@ package com.example.s3supermart.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
 
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -25,6 +31,10 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.MintGreen));
+        }
+
         //intialising ids to variables
         carouselView = findViewById(R.id.carouselView);
         ll_viewAll = findViewById(R.id.ll_viewAll);
@@ -44,6 +54,12 @@ public class HomeActivity extends AppCompatActivity {
         cv_milkCheeseAndYogurt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+         /*       AlertDialog.Builder builder = new AlertDialog.Builder(HomeActivity.this);
+                View view = LayoutInflater.from(HomeActivity.this).inflate(R.layout.dialog_error_message, null);
+                builder.setView(view);
+                AlertDialog dialog = builder.create();
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();*/
                 startActivity(new Intent(HomeActivity.this, MilkCheeseAndYogurtActivity.class).
                         addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
             }

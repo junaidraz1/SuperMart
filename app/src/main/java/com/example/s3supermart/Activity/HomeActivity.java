15aae9e91e81159558e0cf74e6ea63bc60a10ef4
junaidraz1 <junaid87raza@gmail.com>
2022-08-12 14:10:50 +0500

    Added new splash logo and header logo

--- app/src/main/java/com/example/s3supermart/Activity/HomeActivity.java
+++ app/src/main/java/com/example/s3supermart/Activity/HomeActivity.java
@@ -23,43 +23,37 @@ import com.synnapps.carouselview.ImageListener;
 public class HomeActivity extends AppCompatActivity {
 
     CarouselView carouselView;
-    LinearLayout ll_viewAll;
+   // LinearLayout ll_viewAll;
     CardView cv_milkCheeseAndYogurt;
     int[] sampleImages = {R.drawable.offer1, R.drawable.offer2, R.drawable.logo, R.drawable.slider1, R.drawable.offer3};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.home_2);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.MintGreen));
-        }
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//            getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.MintGreen));
+//        }
 
         //intialising ids to variables
         carouselView = findViewById(R.id.carouselView);
-        ll_viewAll = findViewById(R.id.ll_viewAll);
+       // ll_viewAll = findViewById(R.id.ll_viewAll);
         cv_milkCheeseAndYogurt = findViewById(R.id.cv_milkCheeseAndYogurt);
 
 
         carouselView.setPageCount(sampleImages.length);
         carouselView.setImageListener(imageListener);
 
-        ll_viewAll.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(HomeActivity.this, AllCategoriesActivity.class).
-                        addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
-            }
-        });
+//        ll_viewAll.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(HomeActivity.this, AllCategoriesActivity.class).
+//                        addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+//            }
+//        });
         cv_milkCheeseAndYogurt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-         /*       AlertDialog.Builder builder = new AlertDialog.Builder(HomeActivity.this);
-                View view = LayoutInflater.from(HomeActivity.this).inflate(R.layout.dialog_error_message, null);
-                builder.setView(view);
-                AlertDialog dialog = builder.create();
-                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                dialog.show();*/
                 startActivity(new Intent(HomeActivity.this, MilkCheeseAndYogurtActivity.class).
                         addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
             }

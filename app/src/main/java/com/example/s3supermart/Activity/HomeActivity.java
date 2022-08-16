d9911161dd45bc53e6f26351a71eb34e5bac2a90
junaidraz1 <junaid87raza@gmail.com>
2022-08-16 17:58:55 +0500

    Added Home and Wallet fragment. Changed HomeActivity to HomeFragment

--- app/src/main/java/com/example/s3supermart/Activity/HomeActivity.java
+++ app/src/main/java/com/example/s3supermart/Activity/HomeActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -18,6 +19,7 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -28,7 +30,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.s3supermart.Adapter.AllCategoriesAdapter;
+import com.example.s3supermart.Fragment.HomeFragment;
+import com.example.s3supermart.Fragment.MilkAndMilkPowderFragment;
 import com.example.s3supermart.Fragment.MilkCheeseAndYogurtFragment;
+import com.example.s3supermart.Fragment.WalletFragment;
 import com.example.s3supermart.Helper.DialogHandler;
 import com.example.s3supermart.Model.AllCategoriesClass;
 import com.example.s3supermart.R;
@@ -44,60 +49,38 @@ import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
-    CarouselView carouselView;
     BottomNavigationView bottomNavigationView;
-    TextView tv_greetMsg, tv_location;
-    RecyclerView rv_productCategory;
-    ImageView iv_menu;
     FloatingActionButton fab_viewCart;
-    AllCategoriesAdapter allCategoriesAdapter;
-    Utility utility;
-    Fragment fragment;
-    int[] sampleImages = {R.drawable.offer1, R.drawable.offer2,
-            R.drawable.logo, R.drawable.slider1, R.drawable.offer3};
+    TextView tv_location;
+    ImageView iv_menu;
+    int backStack;
+    Fragment HomeFrag, currentFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        HomeFrag = new HomeFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, HomeFrag,
+                HomeFrag.getClass().getSimpleName()).addToBackStack("fragment").commit();
+        Log.d("FRAGMENTTEST", "back stack val on create: " + backStack);
+
+        currentFragment = HomeFrag;
         //intialising ids to variables
-        carouselView = findViewById(R.id.carouselView);
-        tv_greetMsg = findViewById(R.id.tv_gd_morning);
         bottomNavigationView = findViewById(R.id.bottomnavView);
-        tv_location = findViewById(R.id.tv_location);
-        rv_productCategory = findViewById(R.id.rv_productCategory);
-        iv_menu = findViewById(R.id.iv_homeMenu);
         fab_viewCart = findViewById(R.id.fab_viewCart);
+        iv_menu = findViewById(R.id.iv_homeMenu);
+        tv_location = findViewById(R.id.tv_location);
+
+        bottomNavigationView.setBackground(null);
 
         tv_location.setSelected(true);
         tv_location.setEllipsize(TextUtils.TruncateAt.MARQUEE);
 
-        bottomNavigationView.setBackground(null);
-
-        //intialising classes
-        utility = new Utility();
-
-        //method that contains implementation of greeting message
-        greetUser();
-
         //method that contains implementation of click listeners
         clickListeners();
 
-        //to set items to recyclerview for product category
-        setCategory();
-
-        carouselView.setPageCount(sampleImages.length);
-        carouselView.setImageListener(imageListener);
-    }
-
-    //setting layout manager and adapter to category recycler view
-    public void setCategory() {
-
-        GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
-        allCategoriesAdapter = new AllCategoriesAdapter(this, categoryList());
-        rv_productCategory.setAdapter(allCategoriesAdapter);
-        rv_productCategory.setLayoutManager(layoutManager);
     }
 
     //implementation of all the click listeners
@@ -126,87 +109,68 @@ public class HomeActivity extends AppCompatActivity {
                 switch (item.getItemId()) {
 
                     case R.id.homePage:
-                        Toast.makeText(HomeActivity.this, "Clicked", Toast.LENGTH_SHORT).show();
-                        startActivity(new Intent(HomeActivity.this, HomeActivity.class)
-                                .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                        Fragment fragment = new HomeFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, fragment,
+                                fragment.getClass().getSimpleName()).commit();
+                        currentFragment = fragment;
                         break;
 
                     case R.id.wallet:
-                        fragment = new MilkCheeseAndYogurtFragment();
+                        Fragment fragment1 = new WalletFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, fragment1,
+                                fragment1.getClass().getSimpleName()).commit();
+                        currentFragment = fragment1;
                         break;
 
                     case R.id.myorders:
-                        startActivity(new Intent(HomeActivity.this, MyOrdersActivity.class)
-                                .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                        Fragment fragment3 = new MilkCheeseAndYogurtFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, fragment3,
+                                fragment3.getClass().getSimpleName()).commit();
+                        currentFragment = fragment3;
                         break;
 
                     case R.id.search:
-                        startActivity(new Intent(HomeActivity.this, SearchActivity.class)
-                                .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                        Fragment fragment4 = new MilkAndMilkPowderFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, fragment4,
+                                fragment4.getClass().getSimpleName()).commit();
+                        currentFragment = fragment4;
                         break;
                 }
-                getSupportFragmentManager().beginTransaction().replace(R.id.rl_mainLayout, fragment).commit();
                 return true;
             }
         });
     }
 
-    //method to display greeting message on home screen
-    public void greetUser() {
-        if (utility.getWishingMessage() == null) {
-            tv_greetMsg.setText("Good Morning");
-
-        } else {
-            tv_greetMsg.setText(utility.getWishingMessage());
-        }
-    }
-
-    //method to add items to category recycler view
-    public List<AllCategoriesClass> categoryList() {
-
-        List<AllCategoriesClass> allCategoriesList = new ArrayList<>();
-
-        AllCategoriesClass listItem1 = new AllCategoriesClass("Dairy Product", R.drawable.olpers_milk_1_litre);
-        AllCategoriesClass listItem2 = new AllCategoriesClass("Fruits and Vegetables", R.drawable.karaila);
-        AllCategoriesClass listItem3 = new AllCategoriesClass("Noodles and Sauces", R.drawable.milk_cheese_and_yogurt);
-        AllCategoriesClass listItem4 = new AllCategoriesClass("Snacks", R.drawable.lays);
-        AllCategoriesClass listItem5 = new AllCategoriesClass("Ice Cream & Chocolates", R.drawable.pancake_serup);
-        AllCategoriesClass listItem6 = new AllCategoriesClass("Beverages", R.drawable.pancake_serup);
-        AllCategoriesClass listItem7 = new AllCategoriesClass("Personal Care", R.drawable.milk_cheese_and_yogurt);
-        AllCategoriesClass listItem8 = new AllCategoriesClass("Baby Care", R.drawable.milk_cheese_and_yogurt);
-        AllCategoriesClass listItem9 = new AllCategoriesClass("Oil and Ghee", R.drawable.milk_cheese_and_yogurt);
-        AllCategoriesClass listItem10 = new AllCategoriesClass("Atta, Daal aur Chawal", R.drawable.milk_cheese_and_yogurt);
-        AllCategoriesClass listItem11 = new AllCategoriesClass("Laundry", R.drawable.milk_cheese_and_yogurt);
-        AllCategoriesClass listItem12 = new AllCategoriesClass("Tea, Coffee, Sugar", R.drawable.milk_cheese_and_yogurt);
-        AllCategoriesClass listItem13 = new AllCategoriesClass("Baby Food", R.drawable.milk_cheese_and_yogurt);
-
-        allCategoriesList.add(listItem1);
-        allCategoriesList.add(listItem2);
-        allCategoriesList.add(listItem3);
-        allCategoriesList.add(listItem4);
-        allCategoriesList.add(listItem5);
-        allCategoriesList.add(listItem6);
-        allCategoriesList.add(listItem7);
-        allCategoriesList.add(listItem8);
-        allCategoriesList.add(listItem9);
-        allCategoriesList.add(listItem10);
-        allCategoriesList.add(listItem11);
-        allCategoriesList.add(listItem12);
-        allCategoriesList.add(listItem13);
-
-        return allCategoriesList;
-    }
-
-    //image listener for carousel view
-    ImageListener imageListener = new ImageListener() {
-        @Override
-        public void setImageForPosition(int position, ImageView imageView) {
-            imageView.setImageResource(sampleImages[position]);
-        }
-    };
-
     @Override
     public void onBackPressed() {
-        finishAffinity();
+
+        if (currentFragment.equals(HomeFrag)) {
+            finishAffinity();
+
+        } else {
+            currentFragment = HomeFrag;
+            getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, HomeFrag,
+                    HomeFrag.getClass().getSimpleName()).commit();
+            bottomNavigationView.getMenu().getItem(0).setChecked(true);
+
+        }
+
+        //gets value of fragments that are pushed to back stack
+       /* backStack = getSupportFragmentManager().getBackStackEntryCount();
+
+
+        if (backStack >= 2) {
+
+            //pops fragments from backstack
+            getSupportFragmentManager().popBackStack();
+            Log.d("FRAGMENTTEST", "onBackPressed: Inside if");
+            Log.d("FRAGMENTTEST", "back stack: " + backStack);
+
+        } else {
+            finishAffinity();
+            Log.d("FRAGMENTTEST", "onBackPressed: Inside else");
+            Log.d("FRAGMENTTEST", "back stack: " + backStack);
+        } */
+
     }
 }
---
+++ app/src/main/java/com/example/s3supermart/Fragment/HomeFragment.java
@@ -0,0 +1,136 @@
+package com.example.s3supermart.Fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.s3supermart.Adapter.AllCategoriesAdapter;
+import com.example.s3supermart.Model.AllCategoriesClass;
+import com.example.s3supermart.R;
+import com.example.s3supermart.Utils.Utility;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeFragment extends Fragment {
+
+    CarouselView carouselView;
+    TextView tv_greetMsg;
+    RecyclerView rv_productCategory;
+    AllCategoriesAdapter allCategoriesAdapter;
+    Utility utility;
+    int[] sampleImages = {R.drawable.offer1, R.drawable.offer2,
+            R.drawable.logo, R.drawable.slider1, R.drawable.offer3};
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        //intialising id's to variables
+        carouselView = view.findViewById(R.id.carouselView);
+        tv_greetMsg = view.findViewById(R.id.tv_gd_morning);
+        rv_productCategory = view.findViewById(R.id.rv_productCategory);
+
+        //intialising classes
+        utility = new Utility();
+
+        //method that contains implementation of greeting message
+        greetUser();
+
+        //to set items to recyclerview for product category
+        setCategory();
+
+        carouselView.setPageCount(sampleImages.length);
+        carouselView.setImageListener(imageListener);
+
+
+        // Inflate the layout for this fragment
+        return view;
+    }
+
+    //setting layout manager and adapter to category recycler view
+    public void setCategory() {
+
+        GridLayoutManager layoutManager = new GridLayoutManager(getContext(), 2);
+        allCategoriesAdapter = new AllCategoriesAdapter(getContext(), categoryList());
+        rv_productCategory.setAdapter(allCategoriesAdapter);
+        rv_productCategory.setLayoutManager(layoutManager);
+    }
+
+    //method to display greeting message on home screen
+    public void greetUser() {
+        if (utility.getWishingMessage() == null) {
+            tv_greetMsg.setText("Good Morning");
+
+        } else {
+            tv_greetMsg.setText(utility.getWishingMessage());
+        }
+    }
+
+    //method to add items to category recycler view
+    public List<AllCategoriesClass> categoryList() {
+
+        List<AllCategoriesClass> allCategoriesList = new ArrayList<>();
+
+        AllCategoriesClass listItem1 = new AllCategoriesClass("Dairy Product", R.drawable.olpers_milk_1_litre);
+        AllCategoriesClass listItem2 = new AllCategoriesClass("Fruits and Vegetables", R.drawable.karaila);
+        AllCategoriesClass listItem3 = new AllCategoriesClass("Noodles and Sauces", R.drawable.milk_cheese_and_yogurt);
+        AllCategoriesClass listItem4 = new AllCategoriesClass("Snacks", R.drawable.lays);
+        AllCategoriesClass listItem5 = new AllCategoriesClass("Ice Cream & Chocolates", R.drawable.pancake_serup);
+        AllCategoriesClass listItem6 = new AllCategoriesClass("Beverages", R.drawable.pancake_serup);
+        AllCategoriesClass listItem7 = new AllCategoriesClass("Personal Care", R.drawable.milk_cheese_and_yogurt);
+        AllCategoriesClass listItem8 = new AllCategoriesClass("Baby Care", R.drawable.milk_cheese_and_yogurt);
+        AllCategoriesClass listItem9 = new AllCategoriesClass("Oil and Ghee", R.drawable.milk_cheese_and_yogurt);
+        AllCategoriesClass listItem10 = new AllCategoriesClass("Atta, Daal aur Chawal", R.drawable.milk_cheese_and_yogurt);
+        AllCategoriesClass listItem11 = new AllCategoriesClass("Laundry", R.drawable.milk_cheese_and_yogurt);
+        AllCategoriesClass listItem12 = new AllCategoriesClass("Tea, Coffee, Sugar", R.drawable.milk_cheese_and_yogurt);
+        AllCategoriesClass listItem13 = new AllCategoriesClass("Baby Food", R.drawable.milk_cheese_and_yogurt);
+
+        allCategoriesList.add(listItem1);
+        allCategoriesList.add(listItem2);
+        allCategoriesList.add(listItem3);
+        allCategoriesList.add(listItem4);
+        allCategoriesList.add(listItem5);
+        allCategoriesList.add(listItem6);
+        allCategoriesList.add(listItem7);
+        allCategoriesList.add(listItem8);
+        allCategoriesList.add(listItem9);
+        allCategoriesList.add(listItem10);
+        allCategoriesList.add(listItem11);
+        allCategoriesList.add(listItem12);
+        allCategoriesList.add(listItem13);
+
+        return allCategoriesList;
+    }
+
+    //image listener for carousel view
+    ImageListener imageListener = new ImageListener() {
+        @Override
+        public void setImageForPosition(int position, ImageView imageView) {
+            imageView.setImageResource(sampleImages[position]);
+        }
+    };
+
+}
